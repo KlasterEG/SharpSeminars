a5cbbd57192f633b1e1e73c8e42fc92108e1867b
Alexander <Alexander@LOLKEKCHEBYREK>
2023-09-26 18:00:51 +0300

    Домашнее задание по семинару 5

---
+++ Task34/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 34 Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+// [845, 222, 367, 123-> 1
+// (со звездочкой) Отсортировать массив методом пузырька
+
+int[] GenerateNumberArray()
+{
+    var rnd = new Random();
+
+    var lenght = rnd.Next(3, 21); // задаём случайную длину массива от 3 до 20
+    var result = new List<int>();
+
+    for ( int i = 0; i < lenght; i++ )
+    {
+        result.Add(rnd.Next(100, 1000)); //добавляем в массив случайное положительное трехзначное число
+    }
+
+    return result.ToArray();
+}
+
+int CalculateEvenNumbers(int[] numbers)
+{
+    var count = 0;
+
+    foreach ( var number in numbers ) //проверяем на чётность каждое число в массиве с помощью деления на 2 без остатка
+    {
+        if (number % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int[] BubbleSort(int[] array) // сортировка массива методом пузырька
+{
+    /*
+     *  создаём новый массив и копируем в него сгенерированный массив через метод Array.Copy,
+     *  иначе при простом присвоении будет присвоена ссылка на первый массив и все манипуляции с сортировкой 
+     *  будут сделаны в сгенерированном массиве. В итоге получим два одинаковых массива (вернее один массив с двумя ссылками на него) в конце
+     *  */
+
+    var sortArray = new int[array.Length];
+    Array.Copy(array, sortArray, array.Length); 
+   
+    
+    var len = sortArray.Length;
+    for (var i = 1; i < len; i++)
+    {
+        for (var j = 0; j < len - i; j++)
+        {
+            if (sortArray[j] > sortArray[j + 1])
+            {
+                (sortArray[j + 1], sortArray[j]) = (sortArray[j], sortArray[j + 1]); // меняем местами значения элементов с помощью кортежа
+            }
+        }
+    }
+
+    return sortArray;
+}
+
+
+var array = GenerateNumberArray(); // генерируем новый массив
+var evenNumbersCount = CalculateEvenNumbers(array); // считаем количество чётных чисел
+var sortedArray = BubbleSort(array);
+
+
+// отображаем пользователю массив до и после сортировки и количество чётных чисел в нём
+Console.WriteLine("Сгенерирован массив: [{0}]", string.Join(", ", array));
+Console.WriteLine("Сортированный массив [{0}]", string.Join(", ", sortedArray));
+Console.WriteLine($"Количество чётных чисел в массиве: {evenNumbersCount}");
---
+++ Task36/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 36 Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [3, 7, -2, 1]-> 8
+// [-4, -6, 89, 6]-> 0
+// (со звездочкой) Найдите все пары в массиве и выведите пользователю
+
+
+int[] GenerateNumberArray()
+{
+    var rnd = new Random();
+
+    var lenght = rnd.Next(3, 21); // задаём случайную длину массива от 3 до 20
+    var result = new List<int>();
+
+    for (int i = 0; i < lenght; i++)
+    {
+        result.Add(rnd.Next(-100, 101)); //добавляем в массив случайные числа от -100 до 100
+    }
+
+    return result.ToArray();
+    //return new int[] { -1, 2, 1, 4, 6, -7, 12, -2, 3 }; //для удобства проверки тут можно подставить нужный массив, а строку выше закомментировать
+}
+
+int CalculateOddElementSumm(int[] array)
+{
+    var result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+
+    return result;
+}
+
+int[][] FindNumberPairs(int[] array) // будем искать пары противоположных чисел, например -5 и 5
+{
+    var result = new List<List<int>>();
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = i + 1; j < array.Length; j ++)
+        {
+            if (array[j] + array[i] == 0)
+            {
+                result.Add(new List<int>() { array[i], array[j] });
+            }
+        }
+    }
+
+    return result.Select(x=>x.ToArray()).ToArray();
+}
+
+var array = GenerateNumberArray(); // генерируем новый массив
+var summ = CalculateOddElementSumm(array); // считаем сумму нечётных элементов
+var numberPairs = FindNumberPairs(array); // ищем пары чисел
+
+
+// отображаем пользователю массив до и после сортировки и найденные пары чисел в нём
+Console.WriteLine("Сгенерирован массив: [{0}]", string.Join(", ", array));
+Console.WriteLine($"Сумма нечётных чисел в массиве: {summ}");
+
+if (numberPairs.Length == 0)
+{
+    Console.WriteLine("Пары чисел не найдены");
+} else
+{
+    Console.WriteLine("Пары чисел в массиве: [{0}]", string.Join("], [", numberPairs.Select(x=> string.Join(", ", x)).ToArray()));
+}
