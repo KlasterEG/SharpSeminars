422f2a1d285fd74193c65141a079937591b1ff1b
Alexander <Alexander@LOLKEKCHEBYREK>
2023-09-29 18:18:51 +0300

    Домашнее задание по семинару 5

---
+++ Task41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2-> 2
+// - 1, -7, 567, 89, 223-> 3
+// (Задание со звездочкой) Пользователь вводит число нажатий, затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+
+
+int[] ConvertToIntArray(string str)
+{
+    var stringArray = str.Split(','); // разбиваем введённую пользователем строку по запятым
+    var result = new List<int>();
+
+    foreach (var item in stringArray)
+    {
+        var isParsed = int.TryParse(item.Trim(), out var number); // пытаемся парсить каждое число из строки
+
+        if (isParsed) // если получилось, добавляем в результат
+        {
+            result.Add(number);
+        }
+    }
+
+    return result.ToArray();
+}
+
+
+int CalculatePositiveNumbersCount(int[] numbers) // считаем, сколько чисел в массиве больше нуля
+{
+    return numbers.Where(x=>x > 0).Count();
+}
+
+Console.WriteLine("Введите числа через запятую: ");
+var arrayString = Console.ReadLine();
+
+if (string.IsNullOrEmpty(arrayString))
+{
+    Console.WriteLine("Необходимо ввести числа через запятую");
+}
+
+var numbersArray = ConvertToIntArray(arrayString);
+var count = CalculatePositiveNumbersCount(numbersArray);
+
+
+
+// отображаем пользователю спарсенный массив и количество чисел больше нуля
+Console.WriteLine("Введённый массив: [{0}]", string.Join(", ", numbersArray));
+Console.WriteLine($"Количество чисел в массиве больше нуля: {count}");
+
---
+++ Task43/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; 5,5)
+// (Задание со звездочкой) Найдите площадь треугольника образованного пересечением 3 прямых
+
+
+Console.WriteLine("Введите координаты прямых.");
+Console.WriteLine("Введите k1:");
+var k1 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите b1:");
+var b1 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите k2:");
+var k2 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите b2:");
+var b2 = double.Parse(Console.ReadLine() ?? "0");
+
+bool ValidateLineCoordinates(double k1, double b1, double k2, double b2) // проверяем прямые на пересечение
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2) // если все координаты совпадают, то прямые тоже совпадают
+        {
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        }
+         else // если координаты k совпадают, то прямые параллельны и никогда не пересекутся
+        {
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
+
+
+double[] FindCoordinates(double k1, double b1, double k2, double b2)
+{
+    var result = new double[2];
+    result[0] = (b1 - b2) / (k2 - k1);
+    result[1] = k1 * result[0] + k1;
+    return result;
+}
+
+if (ValidateLineCoordinates(k1, b1, k2, b2))
+{
+    var coordinates = FindCoordinates(k1, b1, k2, b2);
+    Console.Write($"Точка пересечения уравнений y={k1}*x+{b1} и y={k2}*x+{b2} имеет координаты ({coordinates[0]};{coordinates[1]})");
+}
